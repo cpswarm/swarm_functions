b17f054c0c8e5aa25e060b6917f516339663be23
Micha Sende <sende@lakeside-labs.com>
2021-10-25 11:57:32 +0200

    improve collision avoidance: better calculation of avoidance magnitude multiple repulsion functions visualization for debugging

--- collision_avoidance/src/lib/repulsion.cpp
+++ collision_avoidance/src/lib/repulsion.cpp
@@ -4,16 +4,17 @@ repulsion::repulsion ()
 {
 }
 
-void repulsion::init (double dist_critical, double dist_avoid)
+void repulsion::init (double dist_critical, double dist_avoid, string repulsion_shape)
 {
     setpoint = CONTROL_UNDEFINED;
     this->dist_critical = dist_critical;
     this->dist_avoid = dist_avoid;
+    this->repulsion_shape = repulsion_shape;
 }
 
 bool repulsion::calc ()
 {
-    // repulsion from other cpss
+    // repulsion from other cpss, normalized to number of neighbors
     geometry_msgs::Vector3 repulsion;
     int neighbors;
     repulse(repulsion, neighbors);
@@ -26,28 +27,28 @@ bool repulsion::calc ()
     // magnitude of repulsion
     double repulsion_mag = hypot(repulsion.x, repulsion.y);
 
-    // direction towards goal position
+    // direction towards goal position, normalized
     geometry_msgs::Vector3 direction = target_direction();
 
-    // avoidance direction
-    geometry_msgs::Vector3 avoidance;
-    avoidance.x = direction.x + repulsion.x;
-    avoidance.y = direction.y + repulsion.y;
+    // avoidance direction normalized
+    this->direction.x = (direction.x + repulsion.x / neighbors) / 2.0;
+    this->direction.y = (direction.y + repulsion.y / neighbors) / 2.0;
 
-    // avoidance magnitude
-    double avoidance_mag = 1 - repulsion_mag / neighbors;
+    // avoidance magnitude, inverse to repulsion to move slower when other cpss close by
+    // linear function, maximum at repulsion zero: dist_avoid, minimum at repulsion one: dist_critical
+    double avoidance_mag = dist_avoid - repulsion_mag / neighbors * (dist_avoid - dist_critical);
 
     // avoidance displacement
     if (setpoint == CONTROL_POSITION) {
-        int_pos.pose.position.x = pos.pose.position.x + avoidance.x * avoidance_mag;
-        int_pos.pose.position.y = pos.pose.position.y + avoidance.y * avoidance_mag;
+        int_pos.pose.position.x = pos.pose.position.x + this->direction.x * avoidance_mag;
+        int_pos.pose.position.y = pos.pose.position.y + this->direction.y * avoidance_mag;
         int_pos.pose.position.z = goal_pos.pose.position.z;
     }
 
     // avoidance velocity
     else if (setpoint == CONTROL_VELOCITY) {
-        int_vel.linear.x = avoidance.x * avoidance_mag;
-        int_vel.linear.y = avoidance.y * avoidance_mag;
+        int_vel.linear.x = this->direction.x * avoidance_mag;
+        int_vel.linear.y = this->direction.y * avoidance_mag;
     }
 
     return true;
@@ -90,6 +91,20 @@ void repulsion::set_swarm (const cpswarm_msgs::ArrayOfVectors::ConstPtr& swarm)
     this->swarm = swarm->vectors;
 }
 
+geometry_msgs::PoseStamped repulsion::get_dir ()
+{
+    // use position of cps
+    geometry_msgs::PoseStamped dir;
+    dir = pos;
+
+    // calculate orientation
+    tf2::Quaternion orientation;
+    orientation.setRPY(0, 0, atan2(direction.y, direction.x));
+    dir.pose.orientation = tf2::toMsg(orientation);
+
+    return dir;
+}
+
 geometry_msgs::PoseStamped repulsion::get_pos ()
 {
     return int_pos;
@@ -127,8 +142,16 @@ void repulsion::repulse (geometry_msgs::Vector3& repulsion, int& neighbors)
                 pot = 1;
 
             // repulsion following sine function
-            else
+            else if (repulsion_shape == "sine")
                 pot = 0.5 - 0.5 * sin(M_PI / (dist_avoid - dist_critical) * (pose.vector.magnitude - 0.5 * (dist_avoid + dist_critical)));
+
+            // repulsion following exp function
+            else if (repulsion_shape == "sine")
+                pot = exp((pose.vector.magnitude - dist_critical) * 2.0*log(0.5) / (dist_avoid - dist_critical));
+
+            // repulsion following linear function
+            else
+                pot = max(1 - (pose.vector.magnitude - dist_critical) / (dist_avoid - dist_critical), 0.0);
 
             // absolute bearing of neighbor
             double bear = yaw + pose.vector.direction;
